fde33704962a4254d9559de5e7dde68908b581dd
Triet Le <triet.lq@holistics.io>
2024-03-05 23:30:11 +0700

    update mar 5

--- lua/soapy/core/keymaps.lua
+++ lua/soapy/core/keymaps.lua
@@ -19,4 +19,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
-
+-- " Use ctrl-[hjkl] to select the active split!
+-- Neovim Lua mapping for window navigation
+vim.keymap.set('n', '<C-K>', ':wincmd k<CR>', { silent = true })
+vim.keymap.set('n', '<C-J>', ':wincmd j<CR>', { silent = true })
+vim.keymap.set('n', '<C-H>', ':wincmd h<CR>', { silent = true })
+vim.keymap.set('n', '<C-L>', ':wincmd l<CR>', { silent = true })
